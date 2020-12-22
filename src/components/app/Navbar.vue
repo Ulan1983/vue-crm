4558d14370f70296fd1ee0d7abf63f7a336314a5
Ulan1983 <ulanbeltaev@gmail.com>
2020-12-22 16:36:20 +0600

    Added current datetime to navbar.

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -8,7 +8,7 @@
         >
           <i class="material-icons black-text">dehaze</i>
         </a>
-        <span class="black-text">12.12.12</span>
+        <span class="black-text">{{date | date('datetime')}}</span>
       </div>
 
       <ul class="right hide-on-small-and-down">
@@ -44,15 +44,29 @@
 
 <script>
 export default {
+  data: () => ({
+    date: new Date(),
+    interval: null,
+    dropdown: null
+  }),
   methods: {
     logout() {
       this.$router.push('/login?message=logout')
     }
   },
   mounted() {
-    window.M.Dropdown.init(this.$refs.dropdown, {
+    this.interval = setInterval(() => {
+      this.date = new Date()
+    }, 1000)
+    this.dropdown = window.M.Dropdown.init(this.$refs.dropdown, {
       constrainWidth: false
     })
+  },
+  beforeDestroy() {
+    clearInterval(this.interval)
+    if (this.dropdown && this.dropdown.destroy) {
+      this.dropdown.destroy()
+    }
   }
 }
 </script>
