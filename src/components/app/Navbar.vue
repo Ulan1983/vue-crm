9c2fcd486d9fa990c5b44fe7bf1f2275af13dc1e
Ulan1983 <ulanbeltaev@gmail.com>
2020-12-22 16:04:23 +0600

    Added router link to Profile component

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -17,6 +17,7 @@
               class="dropdown-trigger black-text"
               href="#"
               data-target="dropdown"
+              ref="dropdown"
           >
             USER NAME
             <i class="material-icons right">arrow_drop_down</i>
@@ -24,13 +25,13 @@
 
           <ul id='dropdown' class='dropdown-content'>
             <li>
-              <a href="#" class="black-text">
+              <router-link to="/profile" class="black-text">
                 <i class="material-icons">account_circle</i>Профиль
-              </a>
+              </router-link>
             </li>
             <li class="divider" tabindex="-1"></li>
             <li>
-              <a href="#" class="black-text">
+              <a href="#" class="black-text" @click.prevent="logout">
                 <i class="material-icons">assignment_return</i>Выйти
               </a>
             </li>
@@ -40,3 +41,18 @@
     </div>
   </nav>
 </template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      this.$router.push('/login?message=logout')
+    }
+  },
+  mounted() {
+    window.M.Dropdown.init(this.$refs.dropdown, {
+      constrainWidth: false
+    })
+  }
+}
+</script>
