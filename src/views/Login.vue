f3bf4f66bcff58c6266dff4a4c738f55362a7dd1
Ulan1983 <ulanbeltaev@gmail.com>
2020-12-23 11:00:14 +0600

    Added displaying message after log out from the system

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -66,6 +66,7 @@
 
 <script>
 import {email, required, minLength} from 'vuelidate/lib/validators'
+import messages from "@/utils/messages";
 export default {
   name: 'login',
   data: () => ({
@@ -75,6 +76,11 @@ export default {
   validations: {
     email: {email, required},
     password: {required, minLength: minLength(6)}
+  },
+  mounted() {
+    if (messages[this.$route.query.message]) {
+      this.$message(messages[this.$route.query.message])
+    }
   },
   methods: {
     submitHandler() {
