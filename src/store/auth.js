57821848acb59ca20239380be7abfb2f01263c3a
Ulan1983 <ulanbeltaev@gmail.com>
2020-12-23 16:58:11 +0600

    Added login function in store

---
+++ src/store/auth.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+
+export default {
+    actions: {
+        async login({dispatch, commit}, {email, password}) {
+            try {
+                console.log(dispatch, commit)
+                await firebase.auth().signInWithEmailAndPassword(email, password)
+            } catch (e) {
+                throw `${e}`;
+            }
+        }
+    }
+}
