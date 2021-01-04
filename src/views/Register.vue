21bf12844f4febf7065c174ae35f0f68e1bf2374
Ulan1983 <ulanbeltaev@gmail.com>
2021-01-04 11:29:11 +0600

    Added async function register, which can add a new user to firebase

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,16 +1,16 @@
 <template>
   <form class="card auth-card" @submit.prevent="submitHandler">
-  <div class="card-content">
-    <span class="card-title">Домашняя бухгалтерия</span>
-    <div class="input-field">
-				<input
-						id="email"
-						type="text"
+    <div class="card-content">
+      <span class="card-title">Домашняя бухгалтерия</span>
+      <div class="input-field">
+        <input
+            id="email"
+            type="text"
             v-model.trim="email"
             :class="{invalid: ($v.email.$dirty && !$v.email.required) || ($v.email.$dirty && !$v.email.email)}"
-				>
-				<label for="email">Email</label>
-				<small
+        >
+        <label for="email">Email</label>
+        <small
             class="helper-text invalid"
             v-if="$v.email.$dirty && !$v.email.required"
         >
@@ -22,16 +22,16 @@
         >
           Введите корректный email
         </small>
-			</div>
-    <div class="input-field">
-				<input
-						id="password"
-						type="password"
+      </div>
+      <div class="input-field">
+        <input
+            id="password"
+            type="password"
             v-model.trim="password"
-						:class="{invalid: ($v.password.$dirty && !$v.password.required) || ($v.password.$dirty && !$v.password.minLength)}"
-				>
-				<label for="password">Пароль</label>
-				<small
+            :class="{invalid: ($v.password.$dirty && !$v.password.required) || ($v.password.$dirty && !$v.password.minLength)}"
+        >
+        <label for="password">Пароль</label>
+        <small
             class="helper-text invalid"
             v-if="$v.password.$dirty && !$v.password.required"
         >
@@ -41,18 +41,19 @@
             class="helper-text invalid"
             v-else-if="$v.password.$dirty && !$v.password.minLength"
         >
-          Пароль должен содержать не менее {{$v.password.$params.minLength.min}} символов. Сейчас он состоит из {{password.length}}.
+          Пароль должен содержать не менее {{ $v.password.$params.minLength.min }} символов. Сейчас он состоит из
+          {{ password.length }}.
         </small>
-			</div>
-    <div class="input-field">
-      <input
-          id="name"
-          type="text"
-          v-model.trim="name"
-          :class="{invalid: ($v.name.$dirty && !$v.name.required) || ($v.name.$dirty && !$v.name.minLength)}"
-      >
-      <label for="name">Имя</label>
-      <small
+      </div>
+      <div class="input-field">
+        <input
+            id="name"
+            type="text"
+            v-model.trim="name"
+            :class="{invalid: ($v.name.$dirty && !$v.name.required) || ($v.name.$dirty && !$v.name.minLength)}"
+        >
+        <label for="name">Имя</label>
+        <small
             class="helper-text invalid"
             v-if="$v.name.$dirty && !$v.name.required"
         >
@@ -62,33 +63,34 @@
             class="helper-text invalid"
             v-else-if="$v.name.$dirty && !$v.name.minLength"
         >
-          Имя должно содержать не менее {{$v.name.$params.minLength.min}} символов. Сейчас оно состоит из {{name.length}}.
+          Имя должно содержать не менее {{ $v.name.$params.minLength.min }} символов. Сейчас оно состоит из
+          {{ name.length }}.
         </small>
+      </div>
+      <p>
+        <label>
+          <input type="checkbox" v-model="agree"/>
+          <span>С правилами согласен</span>
+        </label>
+      </p>
     </div>
-    <p>
-      <label>
-        <input type="checkbox" v-model="agree" />
-        <span>С правилами согласен</span>
-      </label>
-    </p>
-  </div>
-  <div class="card-action">
-    <div>
-      <button
-          class="btn waves-effect waves-light auth-submit"
-          type="submit"
-      >
-        Зарегистрироваться
-        <i class="material-icons right">send</i>
-      </button>
-    </div>
+    <div class="card-action">
+      <div>
+        <button
+            class="btn waves-effect waves-light auth-submit"
+            type="submit"
+        >
+          Зарегистрироваться
+          <i class="material-icons right">send</i>
+        </button>
+      </div>
 
-    <p class="center">
-      Уже есть аккаунт?
-      <router-link to="/login">Войти</router-link>
-    </p>
-  </div>
-</form>
+      <p class="center">
+        Уже есть аккаунт?
+        <router-link to="/login">Войти</router-link>
+      </p>
+    </div>
+  </form>
 </template>
 
 <script>
@@ -109,7 +111,7 @@ export default {
     agree: {checked: val => val}
   },
   methods: {
-    submitHandler() {
+    async submitHandler() {
       if (this.$v.$invalid) {
         this.$v.$touch()
         return
@@ -119,8 +121,13 @@ export default {
         password: this.password,
         name: this.name,
       }
-      console.log(formData);
-      this.$router.push('/login')
+
+      try {
+        await this.$store.dispatch('register', formData)
+        await this.$router.push('/')
+      } catch (e) {
+        console.log(e);
+      }
     }
   }
 }
