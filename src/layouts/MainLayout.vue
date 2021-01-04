e819ba5690e7635f2cda728551462c63e051521b
Ulan1983 <ulanbeltaev@gmail.com>
2021-01-04 14:17:20 +0600

    Added loader to main layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,5 +1,8 @@
 <template>
-	<div class="app-main-layout">
+  <div>
+    <Loader v-if="loading" />
+
+    <div v-else class="app-main-layout">
 
     <Navbar @click="isOpen = !isOpen" />
 
@@ -17,23 +20,29 @@
 			</router-link>
 		</div>
 	</div>
+  </div>
+
 </template>
 
 <script>
 import Navbar from '../components/app/Navbar'
 import Sidebar from '../components/app/Sidebar'
+import Loader from '../components/app/Loader'
 export default {
   name: 'main-layout',
   data: () => ({
-    isOpen: true
+    isOpen: true,
+    loading: true
   }),
   async mounted() {
     if (!Object.keys(this.$store.getters.info).length) {
       await this.$store.dispatch('fetchInfo')
     }
+
+    this.loading = false
   },
   components: {
-    Navbar, Sidebar
+    Navbar, Sidebar, Loader
   }
 }
 </script>
