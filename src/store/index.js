299b88f733cee3838ced7be08ae06d063e31c8e4
Ulan1983 <ulanbeltaev@gmail.com>
2021-01-04 13:50:04 +0600

    Added fetchCurrency action to get currencies

--- src/store/index.js
+++ src/store/index.js
@@ -9,6 +9,13 @@ export default new Vuex.Store({
   state: {
     error: null
   },
+  actions: {
+    async fetchCurrency() {
+      const key = process.env.VUE_APP_FIXER
+      const res = await fetch(`http://data.fixer.io/api/latest?access_key=${key}&symbols=USD,EUR,KGS`)
+      return await res.json()
+    }
+  },
   mutations: {
     setError(state, error) {
       state.error = error
