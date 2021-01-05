55c02c710dc04df24dd4700616c621afdaaeb6c2
Ulan1983 <ulanbeltaev@gmail.com>
2021-01-05 14:52:58 +0600

    Added fetched from database categories to mounted function. Added loader.

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -4,7 +4,8 @@
 			<h3>Категории</h3>
 		</div>
 		<section>
-			<div class="row">
+			<Loader v-if="loading" />
+			<div v-else class="row">
 
 				<CategoryCreate @created="addNewCategory"/>
 
@@ -22,8 +23,13 @@
 	export default {
 		name: 'categories',
 		data: () => ({
-			categories: []
+			categories: [],
+			loading: true
 		}),
+		async mounted() {
+			this.categories = await this.$store.dispatch('fetchCategories');
+			this.loading = false;
+		},
 		methods: {
 			addNewCategory(category) {
 				this.categories.push(category);
