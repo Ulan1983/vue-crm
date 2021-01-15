393cd038bb88d9ea58a56f4fa06e760bf540e11c
Ulan1983 <ulanbeltaev@gmail.com>
2021-01-15 16:43:35 +0600

    Added tooltip position

--- src/directives/tooltip.directive.js
+++ src/directives/tooltip.directive.js
@@ -1,6 +1,6 @@
 export default {
   bind(el, {value}) {
-    window.M.Tooltip.init(el, {html: value})
+    window.M.Tooltip.init(el, {html: value, position: 'top'})
   },
   unbind(el) {
     const tooltip = window.M.Tooltip.getInstance(el)
