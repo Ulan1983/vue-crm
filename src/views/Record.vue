a5dcd1eddf7bc5e2c5a5595ce162ecf7e99ed5be
Ulan1983 <ulanbeltaev@gmail.com>
2021-01-15 12:00:37 +0600

    Added categories to select fetched from database

--- src/views/Record.vue
+++ src/views/Record.vue
@@ -1,65 +1,102 @@
 <template>
   <div>
-  <div class="page-title">
-    <h3>Новая запись</h3>
-  </div>
-
-  <form class="form">
-    <div class="input-field" >
-      <select>
-        <option
-        >name cat</option>
-      </select>
-      <label>Выберите категорию</label>
+    <div class="page-title">
+      <h3>Новая запись</h3>
     </div>
 
-    <p>
-      <label>
+    <Loader v-if="loading" />
+
+    <p class="center" v-else-if="!categories.length">Категорий пока нет. <router-link to="/categories">Добавить категорию</router-link></p>
+
+    <form class="form" v-else>
+      <div class="input-field">
+        <select ref="select" v-model="category">
+          <option
+              v-for="c in categories"
+              :key="c.id"
+              :value="c.id"
+          >{{c.title}}
+          </option>
+        </select>
+        <label>Выберите категорию</label>
+      </div>
+
+      <p>
+        <label>
+          <input
+              class="with-gap"
+              name="type"
+              type="radio"
+              value="income"
+          />
+          <span>Доход</span>
+        </label>
+      </p>
+
+      <p>
+        <label>
+          <input
+              class="with-gap"
+              name="type"
+              type="radio"
+              value="outcome"
+          />
+          <span>Расход</span>
+        </label>
+      </p>
+
+      <div class="input-field">
         <input
-            class="with-gap"
-            name="type"
-            type="radio"
-            value="income"
-        />
-        <span>Доход</span>
-      </label>
-    </p>
+            id="amount"
+            type="number"
+        >
+        <label for="amount">Сумма</label>
+        <span class="helper-text invalid">amount пароль</span>
+      </div>
 
-    <p>
-      <label>
+      <div class="input-field">
         <input
-            class="with-gap"
-            name="type"
-            type="radio"
-            value="outcome"
-        />
-        <span>Расход</span>
-      </label>
-    </p>
-
-    <div class="input-field">
-      <input
-          id="amount"
-          type="number"
-      >
-      <label for="amount">Сумма</label>
-      <span class="helper-text invalid">amount пароль</span>
-    </div>
-
-    <div class="input-field">
-      <input
-          id="description"
-          type="text"
-      >
-      <label for="description">Описание</label>
-      <span
+            id="description"
+            type="text"
+        >
+        <label for="description">Описание</label>
+        <span
             class="helper-text invalid">description пароль</span>
-    </div>
+      </div>
 
-    <button class="btn waves-effect waves-light" type="submit">
-      Создать
-      <i class="material-icons right">send</i>
-    </button>
-  </form>
-</div>
+      <button class="btn waves-effect waves-light" type="submit">
+        Создать
+        <i class="material-icons right">send</i>
+      </button>
+    </form>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'record',
+  async mounted() {
+    this.categories = await this.$store.dispatch('fetchCategories')
+    this.loading = false
+
+    if (this.categories) {
+      this.category = this.categories[0].id
+    }
+
+    setTimeout(() => {
+      this.select = window.M.FormSelect.init(this.$refs.select);
+    }, 0)
+  },
+  data: () => ({
+    loading: true,
+    categories: [],
+    select: null,
+    category: null
+  }),
+  destroyed() {
+			if (this.select && this.select.destroy) {
+				this.select.destroy()
+			}
+		},
+}
+</script>
