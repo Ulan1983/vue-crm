0cec02544da508e00114f7cfb0418edc714fcdff
Ulan1983 <ulanbeltaev@gmail.com>
2021-01-19 13:08:54 +0600

    Added message for requiring to log in.

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,5 +1,6 @@
 export default {
     'logout': 'Вы вышли из системы',
+    'login': 'Для начала работы войдите в систему',
     'auth/user-not-found': 'Такой пользователь не существует',
     'auth/wrong-password': 'Неверный пароль',
     'auth/email-already-in-use': 'Пользователь с таким e-mail уже существует'
