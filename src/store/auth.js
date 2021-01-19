a3e8248e7a56f9b4a8e44b1cfc56e90a3dade0b6
Ulan1983 <ulanbeltaev@gmail.com>
2021-01-19 12:48:32 +0600

    Fixed bug in logout. Didn't redirect to login page.

--- src/store/auth.js
+++ src/store/auth.js
@@ -28,7 +28,7 @@ export default {
       const user = firebase.auth().currentUser
       return user ? user.uid : null
     },
-    async logout(commit) {
+    async logout({commit}) {
       await firebase.auth().signOut()
       commit('clearInfo')
     },
