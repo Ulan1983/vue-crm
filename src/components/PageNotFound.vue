a326f5dff26f0bb1b52db24884891f160d14ce1b
Ulan1983 <ulanbeltaev@gmail.com>
2021-01-20 10:51:13 +0600

    Changed link to main page

--- src/components/PageNotFound.vue
+++ src/components/PageNotFound.vue
@@ -2,8 +2,8 @@
   <div class="container">
     <h1 class="center">404</h1>
     <h5 class="center">
-      Страница не найдена.
-      <router-link to="/login">Нажмите сюда.</router-link>
+      Страница не найдена. Перейдите на
+      <router-link to="/">главную.</router-link>
     </h5>
   </div>
 </template>
