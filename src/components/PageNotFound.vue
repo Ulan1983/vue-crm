129b3afb6b4c897ea017a25788ed398c3f7de0cd
Ulan1983 <ulanbeltaev@gmail.com>
2021-01-23 16:55:52 +0600

    Added localize filter.

--- src/components/PageNotFound.vue
+++ src/components/PageNotFound.vue
@@ -2,8 +2,8 @@
   <div class="container">
     <h1 class="center">404</h1>
     <h5 class="center">
-      Страница не найдена. Перейдите на
-      <router-link to="/">главную.</router-link>
+      Page not found. Go to
+      <router-link to="/">main page.</router-link>
     </h5>
   </div>
 </template>
